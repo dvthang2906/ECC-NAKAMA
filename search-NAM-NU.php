7d207c236a97f70d4ccfafaa8e32fe430cf1cc68
DOVANTHANG <122338950+dvthang2906@users.noreply.github.com>
2023-01-27 17:10:16 +0900

    Update search-NAM-NU.php

--- search-NAM-NU.php
+++ search-NAM-NU.php
@@ -1,6 +1,6 @@
 <?php
 $where = "";
-$where_price = "";
+$where_price = '';
 if(!empty($_SESSION['seach'])){
     $where = $_SESSION['seach'];
 }
@@ -14,10 +14,6 @@ if(!empty($priceRangeTop)){
 if(!empty($priceRangeBotton) && !empty($priceRangeTop)){
     $where_price = "`quanao_price` BETWEEN '$priceRangeBotton' AND '$priceRangeTop'" . ' AND';
 }
-
-// $sql_price = "SELECT * FROM quan_ao WHERE quanao_price BETWEEN '".$_POST["PriceRangeBottom"]."' AND '".$_POST["PriceRangeTop"]."'";
-// printf($sql_price);
-// exit;
 $config_name = "product";
 $config_title = "商品";
 
@@ -26,9 +22,19 @@ $item_per_page = (!empty($_GET['per_page'])) ? $_GET['per_page'] : 10;
 $current_page = (!empty($_GET['page'])) ? $_GET['page'] : 1;
 $offset = ($current_page - 1) * $item_per_page;
 
-$totalRecords = mysqli_query($conn, "SELECT * FROM `quan_ao` WHERE $where_price `name` LIKE '" . $where . "%'");
+if($where == 'L' || $where == 'M'){
+    $totalRecords = mysqli_query($conn, "SELECT * FROM `quan_ao` WHERE $where_price `id_quanao` LIKE '" . $where . "%'");
+$totalRecords = $totalRecords->num_rows;
+$totalPages = ceil($totalRecords / $item_per_page);
+$products = mysqli_query($conn, "SELECT * FROM `quan_ao` WHERE $where_price `id_quanao` LIKE '" . $where . "%' ORDER BY `id` DESC LIMIT " . $item_per_page . " OFFSET " . $offset);
+
+}else{
+    $totalRecords = mysqli_query($conn, "SELECT * FROM `quan_ao` WHERE $where_price `name` LIKE '" . $where . "%'");
 $totalRecords = $totalRecords->num_rows;
 $totalPages = ceil($totalRecords / $item_per_page);
 $products = mysqli_query($conn, "SELECT * FROM `quan_ao` WHERE $where_price `name` LIKE '" . $where . "%' ORDER BY `id` DESC LIMIT " . $item_per_page . " OFFSET " . $offset);
 
+}
+
 $conn->close();
+?>
